aa88d8fd3b1b885a3c85400752582a14f0d5d993
Mizan Ali Khan <mizanpathan3@gmail.com>
2025-08-13 13:31:12 +0530

    Added Hotels Details

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -2,7 +2,7 @@ import { useForm } from "react-hook-form";
 import { useMutation, useQueryClient } from "react-query";
 import * as apiClient from "../api-client";
 import { useAppContext } from "../contexts/AppContext";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 export type LoginFormData = {
   email: string;
@@ -13,6 +13,7 @@ const Login = () => {
   const queryClient = useQueryClient();
   const { showToast } = useAppContext();
   const navigate = useNavigate();
+  const location = useLocation();
   const {
     register,
     handleSubmit,
@@ -24,7 +25,7 @@ const Login = () => {
       console.log("User Logged In");
       showToast({ message: "User Logged In", type: "SUCCESS" });
       await queryClient.invalidateQueries("validateToken");
-      navigate("/");
+      navigate(location.state?.from?.pathname || "/");
     },
     onError: (error: Error) => {
       console.log(error.message);
